ce1b76465908ba35e6399f8670f9c71acc37f462
Julia-yes <ec.ho@mail.ru>
2022-10-14 22:39:04 +0400

    feat: add task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,24 +4,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
- result: [],
-  getLength() {
-    return this.result.length
-  },
-  addLink(value) {
-    this.result.push(value)
-  },
-  removeLink(position) {
-    
-  },
-  reverseChain() {
-    this.result.reverse()
-  },
-  finishChain() {
-    return this.result
-  }
-};
+ const chainMaker = {
+  result: [],
+   getLength() {
+     return this.result.length
+   },
+   addLink(value) {
+     this.result.push(value);
+     return this
+   },
+   removeLink(position) {
+     this.result.splice(position-1, 1);
+     return this
+   },
+   reverseChain() {
+     this.result.reverse();
+     return this
+   },
+   finishChain() {
+    let arr = [];
+    for (item of this.result) {
+      arr.push(`(${item})`)
+    }
+    return arr.join("~~")
+   }
+ };
 
 module.exports = {
   chainMaker
